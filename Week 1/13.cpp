b55e182516c3c04a53ba600a2ee180176bc5e2e2
Tatev <tatev.srapivonyan@gmail.com>
2024-03-13 18:05:05 +0400

    13

---
+++ Week 1/13.cpp
@@ -0,0 +1,33 @@
+//Մուտքագրել թիվ, փոխակերպել թիվը երկուական համակարգի թվի եւ տպել արդյունքը։
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+void to_binary(int num)
+{
+    std::string result {};
+
+    if (num < 2)
+    {
+        result += std::to_string(num);
+    }
+    else 
+    {
+        while (num)
+        {
+            result += std::to_string(num % 2);
+            num /= 2;
+        }
+    }
+
+    std::reverse(result.begin(), result.end());
+
+    std::cout << result << std::endl;
+}
+
+int main()
+{
+    int num = 12;
+    to_binary(num);
+}
