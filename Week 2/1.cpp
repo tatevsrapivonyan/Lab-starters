3240d3934ec625eb3abffd85cc0aa1640474c8dc
Tatev <tatev.srapivonyan@gmail.com>
2024-03-16 16:17:46 +0400

    Week 2

---
+++ Week 2/1.cpp
@@ -0,0 +1,11 @@
+// Գրել ռեկուրսիվ ֆունկցիա, որը ստանում է n բնական թիվ և տպում է 0-ից n թվերը։
+
+void recursive_print(int num)
+{
+    if (num == 0)
+    {
+        return;
+    }
+    recursive_print(--num);
+    std::cout << num << " ";
+}
