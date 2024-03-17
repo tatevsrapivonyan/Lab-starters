167c18f643926b31b5f0e45ae8c7a1aa5bf5f7df
Tatev <tatev.srapivonyan@gmail.com>
2024-03-17 12:42:13 +0400

    Week 3

--- Week 3/10.cpp
+++ Week 3/10.cpp
@@ -2,15 +2,17 @@
 
 #include <iostream>
 
-void array_2d_allocate(int** arr, size_t row, size_t column)
+void array_2d_allocate(int**& arr, size_t row, size_t column)
 {
+    arr = new int*[row];
+
     for (size_t i = 0; i < row; ++i)
     {
         arr[i] = new int[column];
     }
 }
 
-void array_2d_deallocate(int** arr, size_t row, size_t column)
+void array_2d_deallocate(int** arr, size_t row)
 {
     for (size_t i = 0; i < row; ++i)
     {
