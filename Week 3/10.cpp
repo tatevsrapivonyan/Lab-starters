2c097b01a8a514c4a7cdccc0bc782bf42f604429
Tatev <tatev.srapivonyan@gmail.com>
2024-03-17 12:25:09 +0400

    Week 3

---
+++ Week 3/10.cpp
@@ -0,0 +1,20 @@
+// Implement a function to allocate memory for a 2D array dynamically and free it when done.
+
+#include <iostream>
+
+void array_2d_allocate(int** arr, size_t row, size_t column)
+{
+    for (size_t i = 0; i < row; ++i)
+    {
+        arr[i] = new int[column];
+    }
+}
+
+void array_2d_deallocate(int** arr, size_t row, size_t column)
+{
+    for (size_t i = 0; i < row; ++i)
+    {
+        delete[] arr[i];
+    }
+    delete[] arr;
+}
