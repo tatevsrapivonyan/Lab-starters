de32948c5931aaf94fa07bf972a468eab18e3ad2
tatevsrapivonyan <tatev.srapivonyan@gmail.com>
2024-03-25 17:51:12 +0400

    Week 6

---
+++ Week 6/2.cpp
@@ -0,0 +1,25 @@
+// Write a program to read the contents of a text file and display them on the screen.
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::ifstream file;
+    file.open("./files/first.txt");
+    std::string data;
+
+    if (file.is_open())
+    {
+        file >> data;
+        std::cout << data << std::endl;
+        file.close();
+    }
+    else
+    {
+        std::cout << "Error!";
+    }
+
+    return 0;
+}
